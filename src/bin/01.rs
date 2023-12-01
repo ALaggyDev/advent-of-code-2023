fca44acad5b7f2b557eeffd6939f0771d8288aa3
ALaggyDev <renlin456123@gmail.com>
2023-12-01 18:33:02 +0800

    Day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,46 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let mut nums = line.bytes().filter(|c| c.is_ascii_digit()).peekable();
+                ((nums.peek().unwrap() - b'0') * 10 + (nums.last().unwrap() - b'0')) as u32
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    // This is very slow, I know...
+    let new_input = input
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "th3ee")
+        .replace("four", "f4ur")
+        .replace("five", "f5ve")
+        .replace("six", "s6x")
+        .replace("seven", "se7en")
+        .replace("eight", "ei8ht")
+        .replace("nine", "n9ne");
+
+    part_one(&new_input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
